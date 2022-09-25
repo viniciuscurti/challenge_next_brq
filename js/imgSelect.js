b975282a9aaecc84df22bdc1e26f862a59abdbaf
viniciuscurti <vinicius_c_l@hotmail.com>
2022-09-25 19:08:27 -0300

    Corrected errors of sintax and added sound on system_alarm function

--- js/imgSelect.js
+++ js/imgSelect.js
@@ -1,58 +1,73 @@
 
 var imgValidate = false;
+var audioValidate = false;
 
 function alterGarageImg() {
     
     if (imgValidate == true) {
-    document.getElementById("garageId").src="../img/Garage-Open.png";
-    imgValidate = false;
-    }else if (imgValidate == false) {
-    document.getElementById("garageId").src="../img/Garage-Locked.png";
-    imgValidate = true;
+        document.getElementById("garageId").src="./img/Garage-Open.png";
+        imgValidate = false;
+    }else {
+        document.getElementById("garageId").src="./img/Garage-Locked.png";
+        imgValidate = true;
     }
 }
 
 function alterLightImg() {
     
     if (imgValidate == true) {
-    document.getElementById("lightId").src="../img/Light-On.png";
-    imgValidate = false;
-    }else if (imgValidate == false) {
-    document.getElementById("lightId").src="../img/Light-Off.png";
-    imgValidate = true;
+        document.getElementById("lightId").src="./img/Light-On.png";
+        imgValidate = false;
+    }else {
+        document.getElementById("lightId").src="./img/Light-Off.png";
+        imgValidate = true;
     }
 }
 
 function alterDoorImg() {
     
     if (imgValidate == true) {
-    document.getElementById("doorId").src="../img/Door-Open.png";
-    imgValidate = false;
-    }else if (imgValidate == false) {
-    document.getElementById("doorId").src="../img/Door-Closed.png";
-    imgValidate = true;
+        document.getElementById("doorId").src="./img/Door-Open.png";
+        imgValidate = false;
+    }else {
+        document.getElementById("doorId").src="./img/Door-Closed.png";
+        imgValidate = true;
     }
 }
 
 function alterAirConditionerImg() {
     
     if (imgValidate == true) {
-    document.getElementById("airConditionerId").src="../img/Air-Conditioner-On.png";
-    imgValidate = false;
-    }else if (imgValidate == false) {
-    document.getElementById("airConditionerId").src="../img/Air-Conditioner-Off.png";
-    imgValidate = true;
+        document.getElementById("airConditionerId").src="./img/Air-Conditioner-On.png";
+        imgValidate = false;
+    }else {
+        document.getElementById("airConditionerId").src="./img/Air-Conditioner-Off.png";
+        imgValidate = true;
     }
 }
 
 function alterHomeSecurityImg() {
     
     if (imgValidate == true) {
-    document.getElementById("homeSecurityId").src="../img/Home-Security-On.png";
-    imgValidate = false;
-    }else if (imgValidate == false) {
-    document.getElementById("homeSecurityId").src="../img/Home-Security-Off.png";
-    imgValidate = true;
+        document.getElementById("homeSecurityId").src="./img/Home-Security-On.png";
+        imgValidate = false;
+        audioValidate = false
+        play()
+    }else {
+        document.getElementById("homeSecurityId").src="./img/Home-Security-Off.png";
+        imgValidate = true;
+        play()
+        audioValidate = true
     }
 }
 
+
+function play() {
+    var audio = new Audio('./mixkit-police-siren-1641.wav');
+    if (audioValidate == false) {
+        audio.play()
+    }else {
+        audio.pause()
+        audio.currentTime = 0;
+    }
+};
